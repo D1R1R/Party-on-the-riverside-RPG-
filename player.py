3fa28b3c97266d41a5931619a8925f052df6e040
DR <104569323+D1R1R@users.noreply.github.com>
2022-12-09 17:27:12 +0300

    Update player.py
    + бег назад
    + смерть (хп, полученный урон)
    в целях тестирования полученный урон = 100

--- player.py
+++ player.py
@@ -1,4 +1,4 @@
-import math
+import time
 import pygame
 
 from objects import Object
@@ -7,56 +7,97 @@ from objects import Object
 YELLOW = (225, 225, 0)
 BLUE = (0, 0, 255)
 RED = (255, 0, 0)
+GRAY = (72, 77, 76)
+
 
 
 class Player(Object):
-    def field_of_view(self, obj_positions: tuple) -> None:
+    def field_of_view(self, obj_positions: tuple) -> True or False:
         fov = 20  # field_of_view
-        for obj_name, obj_coordinates in obj_positions:
-            obj_x, obj_y = obj_coordinates
-            if obj_x - self._x <= fov // 2:
-                if obj_y - self._y < fov:
-                    obj_name.status("active")
-            elif obj_x - self._x < fov:
-                if obj_y - self._y <= fov // 2:
-                    obj_name.status("active")
-            elif obj_x - self._x == fov and obj_y - self._y == 0:
-                obj_name.status("active")
-            elif obj_y - self._y == fov and obj_x - self._x == 0:
-                obj_name.status("active")
-            else:
-                obj_name.status("passive")
+        return True
+
+    def calculate_hp(self) -> int:
+        hp = 100 - self._taken_dmg
+        if hp <= 0:
+            self._status = "death"
+        return (hp)
+
+    def calculate_hit(self) -> int:
+        atk = 23
+        return atk
+    
+    def take_hit(self) -> None:
+        for mobs in self.nps_list:
+            if self.filed_of_view((mobs._x, mobs._y)):
+                if mobs._atk:
+                    self._taken_dmg += mobs.calculate_hit()
+                    self.calculate_hp()
+
+    def give_hit(self) -> None:
+        self.calculate_hp()
 
     def move(self) -> None:
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_w]: 
-            self._status = "run"
             self._y -= 1
-            self.draw((self._x, self._y), YELLOW)
+            if not self._active:
+                self._status = "run"
+                self._active = True
         
         if keys[pygame.K_d]:
-            self._status = "run"
             self._x += 1
-            self.draw((self._x, self._y), YELLOW)
+            if not self._active:
+                self._status = "run"
+                self._active = True
+            elif self._status == "runback":
+                self._status = "run"
             
         if keys[pygame.K_s]:
-            self._status = "run"
             self._y += 1
-            self.draw((self._x, self._y), YELLOW)
+            if not self._active:
+                self._status = "run"
+                self._active = True
             
         if keys[pygame.K_a]:
-            self._status = "run"
             self._x -= 1
-            self.draw((self._x, self._y), YELLOW)
+            if not self._active:
+                self._status = "runback"
+                self._active = True
+            elif self._status == "run":
+                self._status = "runback"
 
         if (not keys[pygame.K_w] and not keys[pygame.K_d]
             and not keys[pygame.K_s] and not keys[pygame.K_a]):
-            self.draw((self._x, self._y), BLUE)
+            self._active = False
+            self.draw((self._x, self._y), (41, 39, 39))
             self._status = "idle"
+        else:
+            self.draw((self._x, self._y), GRAY)
 
     def update(self):
-        self.move()
-        self.field_of_view(self._support.read_information())
-
-
+        if self._status == "idle" or self._status == "run" or self._status == "runback":
+            self.move()
+        else:
+            self.draw((self._x, self._y), GRAY)
+            if self._frame_index == -1 and self._status == "death":
+                self.game_over((100, 100))
+    
+    def game_over(self, position: tuple) -> None:
+        print("game over!")
+        resurrect = False
+        while not resurrect:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    exit()
+                keys = pygame.key.get_pressed()
+                if keys[pygame.K_RETURN]:
+                    resurrect = True
+                    # reset settings:
+                    self._taken_dmg = 0
+                    self._x, self._y = position
+                    self._frame_index = 0
+                    self._animatoin_speed = 0.10
+                    self._status = "idle"
+                elif keys[pygame.K_x]:
+                    print(22)
