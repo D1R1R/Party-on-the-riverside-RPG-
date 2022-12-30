4958d8e796ed945bea5b478e312e0ff91bc362e9
DR <104569323+D1R1R@users.noreply.github.com>
2022-12-30 21:44:37 +0300

    Update main.py
    добавлено:
    1) пауза 2.0
    2) заставка 
    3)карта

--- main.py
+++ main.py
@@ -1,87 +1,82 @@
 import pygame
-from player import Player
-from map import House
+from player2 import Player
+from mobs import Mob
+from support import Support, Camera
+from map import MapSupport
 
 
 WINDOW_SIZE = (1000, 600)
-FPS = 90
+FPS = 1000
 RED = (255, 0, 0)
-
-def pause(screen) -> None:
-    pause = True
-    while pause:
-        # draw menu
-        pygame.draw.polygon(screen, (121, 85, 72), 
-                    [[(WINDOW_SIZE[0] // 2) + 160, (WINDOW_SIZE[1] // 2) - 200],
-                     [(WINDOW_SIZE[0] // 2) + 160, (WINDOW_SIZE[1] // 2) - 180],
-                     [(WINDOW_SIZE[0] // 2) + 180, (WINDOW_SIZE[1] // 2) - 180],
-                     [(WINDOW_SIZE[0] // 2) + 180, (WINDOW_SIZE[1] // 2) + 180],
-                     [(WINDOW_SIZE[0] // 2) + 160, (WINDOW_SIZE[1] // 2) + 180],
-                     [(WINDOW_SIZE[0] // 2) + 160, (WINDOW_SIZE[1] // 2) + 200],
-                     [(WINDOW_SIZE[0] // 2) - 160, (WINDOW_SIZE[1] // 2) + 200],
-                     [(WINDOW_SIZE[0] // 2) - 160, (WINDOW_SIZE[1] // 2) + 180],
-                     [(WINDOW_SIZE[0] // 2) - 180, (WINDOW_SIZE[1] // 2) + 180],
-                     [(WINDOW_SIZE[0] // 2) - 180, (WINDOW_SIZE[1] // 2) - 180],
-                     [(WINDOW_SIZE[0] // 2) - 160, (WINDOW_SIZE[1] // 2) - 180],
-                     [(WINDOW_SIZE[0] // 2) - 160, (WINDOW_SIZE[1] // 2) - 200],])
-        pygame.draw.polygon(screen, (161, 136, 127), 
-                    [[(WINDOW_SIZE[0] // 2) + 149, (WINDOW_SIZE[1] // 2) - 189],
-                     [(WINDOW_SIZE[0] // 2) + 149, (WINDOW_SIZE[1] // 2) - 169],
-                     [(WINDOW_SIZE[0] // 2) + 169, (WINDOW_SIZE[1] // 2) - 169],
-                     [(WINDOW_SIZE[0] // 2) + 169, (WINDOW_SIZE[1] // 2) + 169],
-                     [(WINDOW_SIZE[0] // 2) + 149, (WINDOW_SIZE[1] // 2) + 169],
-                     [(WINDOW_SIZE[0] // 2) + 149, (WINDOW_SIZE[1] // 2) + 189],
-                     [(WINDOW_SIZE[0] // 2) - 149, (WINDOW_SIZE[1] // 2) + 189],
-                     [(WINDOW_SIZE[0] // 2) - 149, (WINDOW_SIZE[1] // 2) + 169],
-                     [(WINDOW_SIZE[0] // 2) - 169, (WINDOW_SIZE[1] // 2) + 169],
-                     [(WINDOW_SIZE[0] // 2) - 169, (WINDOW_SIZE[1] // 2) - 169],
-                     [(WINDOW_SIZE[0] // 2) - 149, (WINDOW_SIZE[1] // 2) - 169],
-                     [(WINDOW_SIZE[0] // 2) - 149, (WINDOW_SIZE[1] // 2) - 189],])
-        # text
-        font = pygame.font.SysFont('ArialBold', 85)
-        text_surface = font.render("MENU:", False, (212, 175, 55))
-        screen.blit(text_surface,
-                    ((WINDOW_SIZE[0] // 2) - 100, (WINDOW_SIZE[1] // 2) - 189))
-        pygame.display.flip()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                exit()
-            keys = pygame.key.get_pressed()
-            if keys[pygame.K_RETURN]:
-                pause = False
-
 
 def main(size) -> None:
     pygame.init()
     pygame.font.init()
+    sp = Support()
+    camera = Camera()
     clock = pygame.time.Clock()
     width, height = size
 
     pygame.display.set_caption("Прямоугольник")
     pygame.display.set_icon(pygame.image.load('icon.png'))
-    screen = pygame.display.set_mode((width, height))
-    player = Player(screen, (100, 100), "user_1", obj_type="player", path="player_animations/leaf/")
-    # house = House(screen, (500, 300), "house_1", obj_type="player", path="animations/")
-    while True:
-        # ...
+    screen = pygame.display.set_mode((width, height), pygame.RESIZABLE)
+    
+    all_sprites = pygame.sprite.Group()
+
+    visible_group = pygame.sprite.Group()
+    top = pygame.sprite.Group()
+
+    players = pygame.sprite.Group()
+    creatures = pygame.sprite.Group()
+
+    player = Player("idle_1.png", visible_group, all_sprites, players)
+
+    map = MapSupport(top, visible_group, all_sprites)
+    # monk = Mob("monk", "idle_1.png", all_sprites, creatures)
+    map.generate_level(map.load_level("map.txt"))
+    # top
+    map.generate_level(map.load_level("map_top.txt"))
+    running = True
+    sp.game(screen)
+    while running:
+        # events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                exit()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                player.atk("atk")
+                # monk.atk("atk")
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_r]:
+                player.atk("sp_atk")
+                # monk.atk("sp_atk")
+            if keys[pygame.K_p]:
+                sp.pause(screen)
+
         screen.fill((0,0,0))
-        # house.update()
-        player.update()
+        # camera
+        camera.update(player, size) 
+        for sprite in all_sprites:
+            camera.apply(sprite, visible_group, all_sprites)
+        for sprite in top:
+            camera.apply(sprite, top, top)
+        # map
+        visible_group.draw(screen)
+        visible_group.update()
+        # creatures
+        creatures.draw(screen)
+        creatures.update()
+        # PLAYER
+        players.draw(screen)
+        players.update()
+        # top
+        top.draw(screen)
+        top.update()
         
         clock.tick(FPS)
         pygame.display.flip()
           
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                exit()
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                player._frame_index = 0
-                player._status = "atk2"
-                player.give_hit()
-            keys = pygame.key.get_pressed()
-            if keys[pygame.K_p]:
-                pause(screen)
-
-
+                
 if __name__ == '__main__':
     main(WINDOW_SIZE)
