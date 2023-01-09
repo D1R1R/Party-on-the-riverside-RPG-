79560bef7bdc2c00424e0295817cbdbc7bf5d48e
DR <104569323+D1R1R@users.noreply.github.com>
2023-01-09 16:54:16 +0300

    Update player.py
    + game over

--- player.py
+++ player.py
@@ -1,49 +1,61 @@
 import os, sys
 import pygame
+import random
 
 from support import Support
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self,  file_name: str, *group: any) -> None:
+    def __init__(self, pos: tuple, screen,  file_name: str, *group: any) -> None:
         super().__init__(*group)
 
         self._sp = Support()
+        self._sc = screen
 
         self.image = self._sp.load_image('player_animations//leaf', "idle", file_name)
         self.mask = pygame.mask.from_surface(self.image)
-        self.rect = self.image.get_rect()
+        self.rect = self.image.get_rect().move(pos)
         # for animation
         self._status = "idle"
         for animation_name in ["run", "runback", "idle", "sp_atk", "take_hit", "atk", "death", "defend",]:
             self._sp.import_animation('player_animations//leaf', animation_name)
         # specifications
-        self._hp = 100
+        self._act = False
+        self._hp = 10
         self._dmg = 10
+        # for game
+        self.list_of_death = [
+            "you died because you don't play well",
+            "you died because mobs are better than you",
+            "you died, now you have to start over"
+        ]
 
-    def move(self) -> None:
+    def player_move(self, top_group) -> None:
+        old_rect = self.rect
+
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 self._status = "atk"
+                self._act = True
 
         keys = pygame.key.get_pressed()
         if keys[pygame.K_SPACE]: 
             self._status = "defend"
         
         if keys[pygame.K_w]: 
-            self.rect.y -= 1
+            self.rect.y -= 2
             self._status = "run"
         
         if keys[pygame.K_d]:
-            self.rect.x += 1
+            self.rect.x += 2
             self._status = "run"
             
         if keys[pygame.K_s]:
-            self.rect.y += 1
+            self.rect.y += 2
             self._status = "run"
             
         if keys[pygame.K_a]:
-            self.rect.x -= 1
+            self.rect.x -= 2
             self._status = "runback"
         
         if (not keys[pygame.K_w] and not keys[pygame.K_d]
@@ -52,9 +64,64 @@ class Player(pygame.sprite.Sprite):
             and self._status != "sp_atk"):
             self._status = "idle"
 
+        if pygame.sprite.spritecollideany(self, top_group):
+            self.rect = old_rect
+
     def atk(self, atk_type: str) -> None:
         self._status = atk_type
 
-    def update(self, obj=None) -> None:
-        self.image, self._status = self._sp.choose_frame(self._status) 
-        self.move()
+    def hp_line(self):
+        if self._hp <= 0:
+            self._status = "death"
+
+    def game_over(self, screen):
+        game = True
+        bg = pygame.image.load("bg.png")
+        WHITE = (255, 255, 255)
+        phrase = random.choice(self.list_of_death)
+        text = ["GAME OVER :'(",
+        f"{phrase}...",
+        "press <return> to restart!"]
+        while game:
+            window_size = pygame.display.get_window_size()
+            level = (window_size[1] // 2) - 60
+            # bg
+            screen.blit(bg, (0, 0))
+            pygame.draw.polygon(screen, WHITE, 
+                    [[0, window_size[1]], [0, 0],
+                    [window_size[0] // 5, 0], [window_size[0] // 2.5, window_size[1]],])
+            pygame.draw.aalines(screen, WHITE, True, 
+                    [[0, window_size[1]], [0, 0],
+                    [window_size[0] // 5, 0], [window_size[0] // 2.5, window_size[1]],])
+            # text
+            font = pygame.font.SysFont('ArialBold', 85)
+            for sentence in text[:-1]:
+                text_surface = font.render(sentence, False, (212, 175, 55))
+                screen.blit(text_surface,
+                            ((window_size[0] // 5), level))
+                level += 50
+            level += 100
+            font = pygame.font.SysFont('ArialBold', 35)
+            text_surface = font.render(text[-1], False, (212, 175, 55))
+            screen.blit(text_surface,
+                        ((window_size[0] // 5), level))
+            pygame.display.flip()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    exit()
+                keys = pygame.key.get_pressed()
+                if keys[pygame.K_RETURN]:
+                    self._hp = 300
+                    self._status = "idle"
+                    game = False
+
+    def update(self, top_group=False) -> None:
+        if top_group:
+            self.image, self._status = self._sp.choose_frame(self._status) 
+            self.hp_line()
+            if self._status != "death":
+                if self._status != "take_hit":
+                    self.player_move(top_group)
+            else:
+                if self._sp._frame_index == -1:
+                    self.game_over(self._sc)
