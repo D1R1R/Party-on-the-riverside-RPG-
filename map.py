094547726a98304c5d3b6d11ac213faefc18b399
DR <104569323+D1R1R@users.noreply.github.com>
2023-01-09 16:57:37 +0300

    Update map.py
    + изменена карта

--- map.py
+++ map.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from support import Support
+from mobs import Aggressive_Mob, Speaking_Mob
 
 
 
@@ -14,11 +15,13 @@ class MapSupport():
             'wall': sp.load_image('', '', 'box.png'),
             'grass': sp.load_image('tiles', 'down', 'grass_1.png'),
             'flow': sp.load_image('tiles', 'down', 'flowers.png'),
-            'road>': sp.load_image('tiles', 'down', 'road_1.png'),
-            'road^': sp.load_image('tiles', 'down', 'road_2.png'),
+            'empty': sp.load_image('tiles', 'down', 'grass_2.png'),
+            'sea': sp.load_image('tiles', 'down', 'sea.png'),
+            'sea/': sp.load_image('tiles', 'down', 'sea_1.png'),
+            'sea|': sp.load_image('tiles', 'down', 'sea_4.png'),
+            'sea_': sp.load_image('tiles', 'down', 'sea_2.png'),
             'tree1': sp.load_image('tiles', 'medium', 'tree_1.png'),
             'tree2': sp.load_image('tiles', 'medium', 'tree_2.png'),
-            'empty': sp.load_image('tiles', 'down', 'grass_2.png')
         }
 
     def load_level(self, path) -> None:
@@ -38,25 +41,37 @@ class MapSupport():
                     Tile(self._map_images, 'empty', x, y, self._tile_group)
                 elif level[y][x] == '!':
                     Tile(self._map_images, 'grass', x, y, self._tile_group)
+                elif level[y][x] == '&':
+                    Tile(self._map_images, 'sea', x, y, self._tile_group)
                 elif level[y][x] == '@':
                     Tile(self._map_images, 'flow', x, y, self._tile_group)
                 elif level[y][x] == '#':
                     Tile(self._map_images, 'wall', x, y, self._top)
+                elif level[y][x] == '/':
+                    Tile(self._map_images, 'sea/', x, y, self._tile_group)
+                elif level[y][x] == '|':
+                    Tile(self._map_images, 'sea|', x, y, self._tile_group)
                 elif level[y][x] == '_':
-                    Tile(self._map_images, 'road>', x, y, self._tile_group)
-                elif level[y][x] == '^':
-                    Tile(self._map_images, 'road^', x, y, self._tile_group)
+                    Tile(self._map_images, 'sea_', x, y, self._tile_group)
                 elif level[y][x] == '1':
                     Tile(self._map_images, 'tree1', x, y, self._top)
                 elif level[y][x] == '2':
                     Tile(self._map_images, 'tree2', x, y, self._top)
+    def generate_npc(self, level):
+        for y in range(len(level)):
+            for x in range(len(level[y])):
+                # DO NOT USE "+"
+                if level[y][x] == '$':
+                    Aggressive_Mob("monk", "idle_1.png", (x, y), self._top)
+                if level[y][x] == '@':
+                    Speaking_Mob('barmaid', "female", "idle_1.png", (x, y), self._top)
 
 
 
 class Tile(pygame.sprite.Sprite):
     def __init__(self, map_images: dict, tile_type: str, pos_x: int, pos_y: int, *group):
         super().__init__(*group)
-        width = height = 50
+        width = height = 64
         self.image = map_images[tile_type]
         self.rect = self.image.get_rect().move(
             width * pos_x, height * pos_y)
