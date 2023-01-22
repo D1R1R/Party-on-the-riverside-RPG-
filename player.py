fc07936027e017408a5eb5c9a152f2238b072de6
DR <104569323+D1R1R@users.noreply.github.com>
2023-01-22 19:03:33 +0300

    Update player.py
    last player update

--- player.py
+++ player.py
@@ -1,4 +1,3 @@
-import os, sys
 import pygame
 import random
 
@@ -6,19 +5,30 @@ from support import Support
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos: tuple, screen,  file_name: str, *group: any) -> None:
+    """
+    player:
+    move, atk, defend
+    """
+    def __init__(self, pos: tuple, screen,  file_name: str, *group: any) -> None:  # noqa: E501
         super().__init__(*group)
 
         self._sp = Support()
         self._sc = screen
 
-        self.image = self._sp.load_image('player_animations//leaf', "idle", file_name)
+        self.image = self._sp.load_image(
+            'player_animations//leaf', "idle", file_name)
         self.mask = pygame.mask.from_surface(self.image)
         self.rect = self.image.get_rect().move(pos)
         # for animation
         self._status = "idle"
-        for animation_name in ["run", "runback", "idle", "take_hit", "atk", "death", "defend",]:
-            self._sp.import_animation('player_animations//leaf', animation_name)
+        move_list = [
+            "run", "runback", "idle",
+            "take_hit", "atk", "death",
+            "defend",]
+        for animation_name in move_list:
+            self._sp.import_animation(
+                'player_animations//leaf',
+                animation_name)
         # specifications
         self._act = False
         self._hp = 10
@@ -37,28 +47,30 @@ class Player(pygame.sprite.Sprite):
                 self._act = True
 
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_SPACE]: 
+        if keys[pygame.K_SPACE]:
             self._status = "defend"
-        
-        if keys[pygame.K_w]: 
+
+        if keys[pygame.K_w]:
             self.rect.y -= 2
             self._status = "run"
-        
+
         if keys[pygame.K_d]:
             self.rect.x += 2
             self._status = "run"
-            
+
         if keys[pygame.K_s]:
             self.rect.y += 2
             self._status = "run"
-            
+
         if keys[pygame.K_a]:
             self.rect.x -= 2
             self._status = "runback"
-        
-        if (not keys[pygame.K_w] and not keys[pygame.K_d]
+
+        if (
+            not keys[pygame.K_w] and not keys[pygame.K_d]
             and not keys[pygame.K_s] and not keys[pygame.K_a]
-            and self._status != "defend" and self._status != "atk"):
+            and self._status != "defend" and self._status != "atk"
+            ):  # noqa: E125
             self._status = "idle"
 
     def atk(self, atk_type: str) -> None:
@@ -73,20 +85,29 @@ class Player(pygame.sprite.Sprite):
         bg = pygame.image.load("bg.png")
         WHITE = (255, 255, 255)
         phrase = random.choice(self.list_of_death)
-        text = ["GAME OVER :'(",
-        f"{phrase}...",
-        "press <return> to restart!"]
+        text = [
+            "GAME OVER :'(",
+            f"{phrase}...",
+            "press <return> to restart!"]
         while game:
             window_size = pygame.display.get_window_size()
             level = (window_size[1] // 2) - 60
             # bg
             screen.blit(bg, (0, 0))
-            pygame.draw.polygon(screen, WHITE, 
-                    [[0, window_size[1]], [0, 0],
-                    [window_size[0] // 5, 0], [window_size[0] // 2.5, window_size[1]],])
-            pygame.draw.aalines(screen, WHITE, True, 
-                    [[0, window_size[1]], [0, 0],
-                    [window_size[0] // 5, 0], [window_size[0] // 2.5, window_size[1]],])
+            pygame.draw.polygon(
+                screen, WHITE,
+                [
+                    [0, window_size[1]], [0, 0],
+                    [window_size[0] // 5, 0],
+                    [window_size[0] // 2.5, window_size[1]],
+                    ])
+            pygame.draw.aalines(
+                screen, WHITE, True,
+                [
+                    [0, window_size[1]], [0, 0],
+                    [window_size[0] // 5, 0],
+                    [window_size[0] // 2.5, window_size[1]],
+                    ])
             # text
             font = pygame.font.SysFont('ArialBold', 85)
             for sentence in text[:-1]:
@@ -112,7 +133,7 @@ class Player(pygame.sprite.Sprite):
     def update(self, sc=False, boxes_group=False) -> None:
         if boxes_group:
             old_rect_x, old_rect_y = self.rect.x, self.rect.y
-            self.image, self._status = self._sp.choose_frame(self._status) 
+            self.image, self._status = self._sp.choose_frame(self._status)
             self.hp_line(sc)
             if self._status != "death":
                 if self._status != "take_hit":
